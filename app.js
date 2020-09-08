36f29789bc24596341aeaae81501090350b16237
Maylen Nunez <maylennunez@hotmail.com>
2020-09-08 19:58:49 -0400

    posting data to mailchimp via their API

--- app.js
+++ app.js
@@ -2,6 +2,7 @@ const express = require("express");
 const bodyParser = require("body-parser");
 const request = require("request");
 const app = express();
+const https = require("https");
 
 app.use(express.static("public"));
 app.use(bodyParser.urlencoded({extended: true}));
@@ -12,12 +13,57 @@ app.get("/", function(req, res){
 
 app.post("/", function(req, res){
 
-  var firstName = req.body.fName;
-  var lastName = req.body.lName;
-  var email = req.body.email
+  const firstName = req.body.fName;
+  const lastName = req.body.lName;
+  const email = req.body.email;
 
-  console.log(firstName)
+  const data = {
+  members: [
+    {
+       email_address: email,
+       status: "subscribed",
+       merge_fields: {
+         FNAME: firstName,
+         LNAME: lastName
+       }
+    }
+]
+};
+const jsonData = JSON.stringify(data);
+
+const url = "https://us17.api.mailchimp.com/3.0/lists/1769d48af7";
+
+const options = {
+  method: "POST",
+
+}
+
+var request = https.request(url, options, function(response) {
+
+if   (response.statusCode === 200){
+  res.send("Susccessfully subscribed!");
+
+} else {
+  res.send("There was an error with signing up, please try again!");
+}
+
+
+  response.on("data", function(data){
+  console.log(JSON.parse(data));
+  })
 })
+
+request.write(jsonData);
+request.end();
+
+
+})
+
+
+
+
+
+// List ID 1769d48af7
 
  app.listen(3000, function(){
   console.log("Server is running on port 3000")
